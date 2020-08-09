3a255297bba6594796e652e7c0ec3ad72a2fbcd4
kubatyi <nazar.kubaty@gmail.com>
2020-08-09 15:52:46 +0300

    initial

---
+++ precache-manifest.3be610cbaeb9327b642ac1cabd2ed539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5ce3332b827ed93102fcb72a4f6a39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50f41b1f176ceeff023d",
+    "url": "/static/css/main.18bb83b2.chunk.css"
+  },
+  {
+    "revision": "55fed809ea265a55d4c4",
+    "url": "/static/js/2.952caa71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.952caa71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50f41b1f176ceeff023d",
+    "url": "/static/js/main.5f1894fb.chunk.js"
+  },
+  {
+    "revision": "a918736c91ee01e7d228",
+    "url": "/static/js/runtime-main.7c6f3319.js"
+  }
+]);
